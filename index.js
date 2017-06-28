b3c95d681becceedd73f6ff4fadc2aa499e87a68
Chris Burke <burkech@umail.iu.edu>
2017-06-28 11:42:58 -0500

    Day 2 Homework (incomplete)

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-function changeDivContent(ev) {
-    ev.preventDefault()
-    const f = ev.target
-    const name = f.personName.value
-    const div = document.querySelector('#stats')
-div.innerHTML = name.fontcolor("blue").fontsize(10) 
+function handleSubmit(ev) {
+  ev.preventDefault()
+  const f = ev.target
+  const name = f.personName.value
+  const age = f.personAge.value
+  const color = f.favoriteColor.value
+
+  const div = document.querySelector('#stats')
+  const list = document.createElement('ul')
+  div.appendChild(list)
+
+const nameItem = renderListItem('Name', name)
+list.appendChild(nameItem)
+
+const ageItem = renderListItem('Age', age)
+list.appendChild(ageItem)
+
+const colorDiv = renderColor(color)
+const colorItem = renderListItem('Favorite Color', colorDiv)
+list.appendChild(colorItem)
+
 }
 
-
 const personForm = document.querySelector('#person-form')
-personForm.addEventListener('submit', changeDivContent)
+personForm.addEventListener('submit', handleSubmit)
+
+
+function renderColor(color) {
+  const colorDiv = document.createElement('div')
+  colorDiv.style.backgroundColor = color
+  colorDiv.style.width = '6rem'
+  colorDiv.style.height = '3rem'
+  return colorDiv
+}
+
+function renderListItem(label, value) {
+       
+    const listItem = document.createElement('li')
+ if (typeof value === 'string') {
+     listItem.textContent = `${label}: ${value}`
+  } else {
+      listItem.textContent = `${label}: `
+      listItem.appendChild(value)
+  }
+ 
+  return listItem
+
+}
